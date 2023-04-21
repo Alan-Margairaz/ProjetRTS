f1278059ecd3e213e479d942e75a2edcc7101ab1
Alan Margairaz <apmargairaz@gmail.com>
2023-04-21 14:21:46 +0200

    Template soldat

--- inc/Soldier.h
+++ inc/Soldier.h
@@ -3,3 +3,26 @@
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
+
+using namespace std;
+
+class Soldier
+{
+private:
+	int hp_;
+	int armor_;
+
+
+public:
+	Soldier();
+	~Soldier();
+	Soldier(const Soldier& src);
+	Soldier& operator=(const Soldier& rhs);
+
+	void init();
+	void move();
+	void upgrade();
+	void kill();
+
+	int& get_time();
+};
--- src/Soldier.cpp
+++ src/Soldier.cpp
@@ -1,3 +1,25 @@
 #include 'Soldier.h'
 
 #include <iostream>
+
+using namespace std;
+
+Soldier::Soldier()		// Constructeur, création de l'objet
+{
+
+}
+
+Soldier::~Soldier()		// Destruction de l'objet
+{
+
+}
+
+Soldier::Soldier(const Soldier& src)	// Copie de la classe
+{
+
+}
+
+Soldier &Soldier::operator=(const Soldier& rhs)		// Instanciation de la copie crée
+{
+
+}
