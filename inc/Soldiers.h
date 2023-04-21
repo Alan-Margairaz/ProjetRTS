da99b2d79ab5912738ece8f8d8e4ea42a5d7778d
Alan Margairaz <apmargairaz@gmail.com>
2023-04-21 16:59:38 +0200

    Soldier class implementation
    TODO list in the comments at the top of Soldiers.cpp

--- inc/Soldiers.h
+++ inc/Soldiers.h
@@ -3,26 +3,35 @@
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Soldiers
 {
-private:
-	int hp_;
-	int armor_;
-
+protected:
+	int _hp, _moveDistance, _level, _damage;
+	vector<int> _placement;
+	string _name;
 
 public:
-	Soldiers();
+	Soldiers(int _hp, int _moveDistance, int _level, int _damage);
 	~Soldiers();
-	Soldiers(const Soldier& src);
+	Soldiers(const Soldiers& src);
 	Soldiers& operator=(const Soldiers& rhs);
 
-	void create();
-	void move();
-	void upgrade();
-	void kill();
+	void move(int _moveDistance);
+	void upgrade(int _level, int _moveDistance, int _hp, string _name);
+	void killEnemy(Soldiers& _soldiers, int _damage, int _level);
+	void destroy();
 
-	int& get_time();
+	void setHP(int new_hp);
+	const int getHP();
+
+	void setName(string new_name);
+	const string getName();
+
+	void setPlacement(vector<int> new_placement);
+	const vector<int> getPlacement();
 };
--- src/Soldiers.cpp
+++ src/Soldiers.cpp
@@ -4,50 +4,92 @@
 
 using namespace std;
 
-// Constructeur, création de l'objet
-Soldiers::Soldiers()		
+///		SOLDIER BASE CLASS 
+///
+///		TODO : implement destroy(), pointers to ennemies & walls to apply damage, implement map movement
+///		TODO : implement child classes for soldiers lvl 1,2 & 3
+
+
+// Constructor, creation of the oject
+Soldiers::Soldiers(int _hp, int _moveDistance, int _level, int _damage)
 {
 
 }
 
-// Destruction de l'objet
+// Objects' destruction
 Soldiers::~Soldiers()		
 {
 
 }
 
-// Copie de la classe
+// Copy of the class
 Soldiers::Soldiers(const Soldiers& src)	
 {
 
 }
 
-// Instanciation de la copie crée
+// Instanciation of the copied class
 Soldiers &Soldiers::operator=(const Soldiers& rhs)		
 {
 
 }
 
-// initialisation du soldat avec ses caractéristiques & placement sur le terrain
-void create()
+// Soldier's movement on the map
+void Soldiers::move(int _moveDistance)
+{
+
+}
+
+// Upgrade soldiers' rank
+void Soldiers::upgrade(int _level, int _moveDistance, int _hp, string _name)
+{
+
+}
+
+// Condition check if killing the enemy is possible or not:
+void Soldiers::killEnemy(Soldiers& _soldiers, int _damage, int _level)
+{
+	
+	_soldiers.setHP(_soldiers.getHP() - _damage);
+
+	if (_soldiers.getHP() <= 0)
+		_soldiers.destroy();
+}
+
+// Destruction method for the attacked ennemy:
+void Soldiers::destroy()
 {
 	
 }
 
-// Déplacement du soldat par cases en fonction de son niveau
-void move()
+// HP setter & getter:
+void Soldiers::setHP(int new_hp)
 {
-	
+	_hp = new_hp;
+}
+const int Soldiers::getHP()
+{
+	return _hp;
 }
 
-// Amélioration du soldat du niveau 1 à 2 puis 2 à 3
-void upgrade()
+// Name setter & getter:
+void Soldiers::setName(string new_name)
 {
-	
+	_name = new_name;
+}
+const string Soldiers::getName()
+{
+	return _name;
 }
 
-// Action d'attaque du soldat
-void kill()
+// Placement on the map setter & getter:
+void Soldiers::setPlacement(vector<int> new_placement)
 {
-	
+	_placement = new_placement;
 }
+const vector<int> Soldiers::getPlacement()
+{
+	return _placement;
+}
+
+
